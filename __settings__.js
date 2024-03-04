7b9e3f06f62525bc498229422392ac0e7e64e8d1
d0sob <117089683+d0sob@users.noreply.github.com>
2024-03-04 21:36:18 +0530

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1713475.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 128511106, 128688300, 128688301, 128688302, 128693664, 128800384, 135691530 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/128511093/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/128511094/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/128511092/1/ammo.js', 'preload' : true},
+];
